436a734777fa1202a5684fd5cc540db862ec1b36
GebbyT <tesfayegebrehiwot123@ethioware.org>
2024-07-22 13:51:34 +0300

    partally merged

--- Frontend/sign-in_sign-up/homepage.js
+++ Frontend/sign-in_sign-up/homepage.js
@@ -20,50 +20,50 @@ const firebaseConfig = {
   messagingSenderId: "811013712823",
   appId: "1:811013712823:web:4a4ebc4ded8f6755e55632",
 };
- 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
 
-  const auth=getAuth();
-  const db=getFirestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  onAuthStateChanged(auth, (user)=>{
-    const loggedInUserId=localStorage.getItem('loggedInUserId');
-    if(loggedInUserId){
-        console.log(user);
-        const docRef = doc(db, "users", loggedInUserId);
-        getDoc(docRef)
-        .then((docSnap)=>{
-            if(docSnap.exists()){
-                const userData=docSnap.data();
-                document.getElementById('loggedUserFName').innerText=userData.firstName;
-                document.getElementById('loggedUserEmail').innerText=userData.email;
-                document.getElementById('loggedUserLName').innerText=userData.lastName;
-                document.getElementById('loggedUserUName').innerText=userData.userName;
+const auth = getAuth();
+const db = getFirestore();
 
-            }
-            else{
-                console.log("no document found matching id")
-            }
-        })
-        .catch((error)=>{
-            console.log("Error getting document");
-        })
-    }
-    else{
-        console.log("User Id not Found in Local storage")
-    }
-  })
+onAuthStateChanged(auth, (user) => {
+  const loggedInUserId = localStorage.getItem("loggedInUserId");
+  if (loggedInUserId) {
+    console.log(user);
+    const docRef = doc(db, "users", loggedInUserId);
+    getDoc(docRef)
+      .then((docSnap) => {
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          document.getElementById("loggedUserFName").innerText =
+            userData.firstName;
+          document.getElementById("loggedUserEmail").innerText = userData.email;
+          document.getElementById("loggedUserLName").innerText =
+            userData.lastName;
+          document.getElementById("loggedUserUName").innerText =
+            userData.userName;
+        } else {
+          console.log("no document found matching id");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document");
+      });
+  } else {
+    console.log("User Id not Found in Local storage");
+  }
+});
 
-  const logoutButton=document.getElementById('logout');
+const logoutButton = document.getElementById("logout");
 
-  logoutButton.addEventListener('click',()=>{
-    localStorage.removeItem('loggedInUserId');
-    signOut(auth)
-    .then(()=>{
-        window.location.href='index.html';
+logoutButton.addEventListener("click", () => {
+  localStorage.removeItem("loggedInUserId");
+  signOut(auth)
+    .then(() => {
+      window.location.href = "index.html";
     })
-    .catch((error)=>{
-        console.error('Error Signing out:', error);
-    })
-  })
+    .catch((error) => {
+      console.error("Error Signing out:", error);
+    });
+});
